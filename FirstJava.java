5914deb62d33b8fe5309d3938aa034f23139cb6d
Zakia <96790443+zakiadaniel@users.noreply.github.com>
2024-07-12 16:08:27 -0700

    HelloWorld.java

--- FirstJava.java
+++ FirstJava.java
@@ -1,3 +1,6 @@
-#Display the output
+public class HelloWorld {
 
-  System.out.println ("Hello World!);
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
