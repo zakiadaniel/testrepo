b719e32a280d658c7107f251ef57a3218d40cf06
Zakia <96790443+zakiadaniel@users.noreply.github.com>
2024-07-12 16:11:04 -0700

    App.java

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     
     public static void main(String[] args) {
         System.out.println("This is a simple Java application.");
+        System.out.println("What does this feature do?");
     }
 }
